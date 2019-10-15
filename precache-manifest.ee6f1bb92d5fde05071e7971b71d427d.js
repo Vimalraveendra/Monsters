5990c034a028355507f7c1ac59e6fc2d6498fd2f
vimal Raveendran <vimalraveendra@gmail.com>
2019-10-15 17:00:21 +0200

    Updates

---
+++ precache-manifest.ee6f1bb92d5fde05071e7971b71d427d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc278edb49aacf9a11ec9f4179331ee0",
+    "url": "/Monsters-app/index.html"
+  },
+  {
+    "revision": "6962d70aa7203248a36a",
+    "url": "/Monsters-app/static/css/main.368a82b4.chunk.css"
+  },
+  {
+    "revision": "0b7299f79c7bc9f3b8c9",
+    "url": "/Monsters-app/static/js/2.3e8d7f07.chunk.js"
+  },
+  {
+    "revision": "6962d70aa7203248a36a",
+    "url": "/Monsters-app/static/js/main.041e5a78.chunk.js"
+  },
+  {
+    "revision": "d41bfa80c52f04d2326c",
+    "url": "/Monsters-app/static/js/runtime-main.4a07b880.js"
+  }
+]);
