831018f86be79652bd4258b27550de76cc74b98c
vimal Raveendran <vimalraveendra@gmail.com>
2019-10-15 16:20:14 +0200

    Updates

---
+++ precache-manifest.348bf774bc18500f40a7a7c2b35c9ad0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5324133d0dc84f16682a46bd3cf16dff",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "678ca566b1c3e1813c93",
+    "url": "/monsters/static/css/main.54b6eccd.chunk.css"
+  },
+  {
+    "revision": "e9903f85e2d9b0193bca",
+    "url": "/monsters/static/js/2.3f22b8ec.chunk.js"
+  },
+  {
+    "revision": "678ca566b1c3e1813c93",
+    "url": "/monsters/static/js/main.c5266fdf.chunk.js"
+  },
+  {
+    "revision": "550737b1e9cefa126497",
+    "url": "/monsters/static/js/runtime-main.21728a22.js"
+  }
+]);
