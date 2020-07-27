a67af39312178b1c70b860819e68b9657edeea16
vimal Raveendran <vimalraveendra@gmail.com>
2020-07-27 21:27:49 +0200

    updated package dot json

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,11 +1,10 @@
 import { shallow } from "enzyme";
 import React from "react";
 import App from "./App";
-<<<<<<< HEAD
+
 import fetchMock from "fetch-mock";
-=======
+
 // import fetchMock from "fetch-mock";
->>>>>>> e9fc009b35bf3854b516c4981e87ae6a75bb69b6
 
 let wrapper;
 beforeEach(() => {
@@ -20,48 +19,39 @@ describe("handleChange", () => {
   it("should handle the change and setstate after recieving the input", () => {
     const mockProps = {
       target: {
-<<<<<<< HEAD
-        value: "hel"
-      }
-=======
         value: "hel",
       },
->>>>>>> e9fc009b35bf3854b516c4981e87ae6a75bb69b6
     };
 
     const expectedAction = {
       monsters: [],
-<<<<<<< HEAD
-      searchField: "hel"
-=======
       searchField: "hel",
->>>>>>> e9fc009b35bf3854b516c4981e87ae6a75bb69b6
     };
     wrapper.instance().handleChange(mockProps);
     expect(wrapper.state()).toEqual(expectedAction);
   });
 });
-<<<<<<< HEAD
-describe("fetchAPI", () => {
-  it("should call fetchApi to get the monsters", async () => {
-    const mockProps = {
-      monsters: [
-        {
-          id: 123,
-          name: "hello",
-          email: "hello@gmail.com"
-        }
-      ]
-    };
 
-    fetchMock
-      .getOnce("https://jsonplaceholder.typicode.com/users", mockProps)
-      .then(() => {
-        expect(wrapper.state()).toEqual("hello");
-      });
-  });
-});
-=======
+// describe("fetchAPI", () => {
+//   it("should call fetchApi to get the monsters", async () => {
+//     const mockProps = {
+//       monsters: [
+//         {
+//           id: 123,
+//           name: "hello",
+//           email: "hello@gmail.com",
+//         },
+//       ],
+//     };
+
+//     fetchMock
+//       .getOnce("https://jsonplaceholder.typicode.com/users", mockProps)
+//       .then(() => {
+//         expect(wrapper.state()).toEqual("hello");
+//       });
+//   });
+// });
+
 // describe("fetchAPI", () => {
 //   it("should call fetchApi to get the monsters", async () => {
 //     const mockProps = {
@@ -81,4 +71,3 @@ describe("fetchAPI", () => {
 //       });
 //   });
 // });
->>>>>>> e9fc009b35bf3854b516c4981e87ae6a75bb69b6
