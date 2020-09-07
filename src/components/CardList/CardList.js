a0a40247714cd256326ad6507838d84e1590f848
vimal Raveendran <vimalraveendra@gmail.com>
2020-09-07 17:39:53 +0200

    updated serialize-javascript library

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,25 +1,22 @@
-import React from  'react';
-import  Card  from '../Card/Card';
-import './CardList.css';
+import React from "react";
+import Card from "../Card/Card";
+import "./CardList.css";
 
-const CardList = ({monsters}) =>{
-
-    return(
-            <div className='card-list'>
-            {
-                monsters.map(monster=>{
-               return  <Card  key={monster.id}
-               id={monster.id}
-               name= {monster.name} 
-               email={monster.email}
-               />
-            })
-        
-        }
-            </div>
-        
-    
-    )
-}
+const CardList = ({ monsters }) => {
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => {
+        return (
+          <Card
+            key={monster.id}
+            id={monster.id}
+            name={monster.name}
+            email={monster.email}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default CardList;
